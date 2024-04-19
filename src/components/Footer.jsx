79f4a49c381f5db367e3a1cd96d9e987a847f111
Malcon <malcon.heltner@gmail.com>
2024-04-19 17:49:41 -0300

    Refactor updating the year in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./Footer.css";
+import { useEffect } from "react";
 
 export function Footer() {
-  const [year, setYear] = useState(() => new Date().getFullYear());
+  const [year, setYear] = useState();
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    setYear(currentYear);
+  }, []);
 
   return (
     <footer className="footer">
