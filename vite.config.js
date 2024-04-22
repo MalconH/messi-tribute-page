b460305e0b0cb27c9278dfaac3215cf2641b3ee7
Malcon <malcon.heltner@gmail.com>
2024-04-22 11:36:53 -0300

    Fix error in project base
    When deployed, the server was looking for assets in the wrong URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://malconh.github.io/",
+  base: "https://malconh.github.io/messi-tribute-page",
 });
